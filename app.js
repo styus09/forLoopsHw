52a87305573a3a50c1de2bc47455d6c25d3de2bd
Sean <styus09@gmail.com>
2021-10-21 00:28:26 -0400

    forLoopsHw

---
+++ app.js
@@ -0,0 +1,25 @@
+var myArray = [];
+for (var i = 1; i < 11; i++) {
+    myArray.push(i);
+}
+
+var myArray = [];
+var i = 10;
+while(i > 0){
+    myArray.push(i);
+    i--;
+}
+
+var myArray = [];
+var i = 11; 
+while(i < 22) {
+    myArray.push(i);
+    i += 3;
+}
+
+var myArray = [];
+var i = 15; 
+while(i > 0) {
+    myArray.push(i);
+    i -= 2;
+}
